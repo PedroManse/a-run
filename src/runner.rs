b4ca970cafc5ea1810e3955bf17e21637ef74763
Manse <pedro.manse@dmk3.com.br>
2025-09-03 14:53:51 -0300

    runner: removed ExecuteMessage, useless trait; renamed Runner creator

--- src/runner.rs
+++ src/runner.rs
@@ -19,20 +19,6 @@ where
     Send(SendError<Res>),
 }
 
-pub trait ExecuteMessage {
-    type Res;
-    fn execute(self) -> Self::Res;
-}
-
-/// Implement ControlExecuteMessage for requests that implement ExecuteMessage.
-/// This should only be used for prototyping
-impl<E: ExecuteMessage> ControlExecuteMessage for E {
-    type Res = <E as ExecuteMessage>::Res;
-    fn execute(self) -> ControlFlow<(), Self::Res> {
-        ControlFlow::Continue(<Self as ExecuteMessage>::execute(self))
-    }
-}
-
 pub trait ControlExecuteMessage {
     type Res;
     fn execute(self) -> ControlFlow<(), Self::Res>;
@@ -45,21 +31,18 @@ where
     Req: ControlExecuteMessage + Send + Sync + 'static,
     <Req as ControlExecuteMessage>::Res: std::fmt::Debug + Send + 'static,
 {
-    pub fn new() -> (Sender<Req>, Receiver<Ret<Req>>) {
+    pub fn make_unbound() -> (Sender<Req>, Receiver<Ret<Req>>) {
         let (res_send, res_recv) = mpsc::channel();
         let (req_send, req_recv) = mpsc::channel();
-        Self::make_runner(req_recv, res_send);
+        Self::make_bound(req_recv, res_send);
         (req_send, res_recv)
     }
-    pub fn make_runner(
-        req_recv: Receiver<Req>,
-        res_send: Sender<Ret<Req>>,
-    ) {
+    pub fn make_bound(req_recv: Receiver<Req>, res_send: Sender<Ret<Req>>) -> std::thread::JoinHandle<()> {
         Runner {
             incoming: req_recv,
             outgoing: res_send,
         }
-        .run_thread();
+        .run_thread()
     }
     pub fn run_thread(mut self) -> std::thread::JoinHandle<()> {
         std::thread::spawn(move || while self.execute_one().unwrap().is_continue() {})
@@ -69,8 +52,7 @@ where
         let res = msg.execute();
         Ok(match res {
             ControlFlow::Continue(m) => {
-                let msg: Ret<Req> = m.into();
-                self.outgoing.send(msg.into()).map_err(RunnerError::Send)?;
+                self.outgoing.send(m).map_err(RunnerError::Send)?;
                 ControlFlow::Continue(())
             }
             ControlFlow::Break(()) => ControlFlow::Break(()),
